28bfc3ea4589b3656c3978cc0988330b03403961
Michael Ketchel <mdk@rgnets.com>
2024-08-20 17:41:08 -0400

    Added instanceable behavior for namespacing

--- wlanpi_core/services/network/namespace/models/network_namespace.py
+++ wlanpi_core/services/network/namespace/models/network_namespace.py
@@ -10,58 +10,104 @@ from wlanpi_core.utils.general import run_command
 # log = logging.getLogger("uvicorn")
 
 class NetworkNamespace():
-    _logger = logging.getLogger("NetworkNamespace")
+    _static_logger = logging.getLogger("NetworkNamespace")
     def __init__(self, name: str ):
         # super().__init__(error_msg)
-
         self.name = name
+        self.creation_result = None
+        self.log = logging.getLogger("NetworkNamespace")
+
+        if not NetworkNamespace.namespace_exists(name):
+            self.creation_result = NetworkNamespace.create(name)
+
+    def get_interfaces(self) -> list:
+        """
+        Returns all interfaces that belong to this network namespace, a la ifconfig
+        """
+        return NetworkNamespace.get_interfaces_in_namespace(self.name)
+
+    def get_processes(self) -> list:
+        """
+        Lists all processes currently running in a namespace
+        """
+        return NetworkNamespace.processes_using_namespace(self.name)
+
+    def destroy(self):
+        """
+        Destroys this network namespace, killing all processes inside and
+        moving interfaces back to the root namespace as necessary.
+        """
+        return NetworkNamespace.destroy_namespace(self.name)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        try:
+            self.destroy()
+        except NetworkNamespaceNotFoundError:
+            # User may have already called destroy here.
+            pass
+
+    # Static methods
 
     @staticmethod
-    def create(name: str) -> CommandResult:
+    def create(namespace_name: str) -> CommandResult:
         """
         Attempts to create a network namespace
         """
-        return run_command(f"ip -j netns add {name}".split())
+        return run_command(f"ip -j netns add {namespace_name}".split())
 
     @staticmethod
     def list_namespaces() -> list:
-        """ Lists all known network namespaces """
+        """
+        Lists all known network namespaces
+        """
         result = run_command(f"ip -j netns list".split(), raise_on_fail=False)
         if not result.success:
             raise NetworkNamespaceError(f"Error listing namespaces: {result.error}")
         return result.output_from_json() or []
 
     @staticmethod
-    def namespace_exists(name: str) -> bool:
+    def namespace_exists(namespace_name: str) -> bool:
+        """
+        Indicates if a network namespace exists
+        """
         namespaces = NetworkNamespace.list_namespaces()
-        return name in [ns['name'] for ns in namespaces]
+        return namespace_name in [ns['name'] for ns in namespaces]
 
     @staticmethod
-    def get_interfaces_in_namespace(name: str) -> list:
-        return run_command(f"ip netns exec {name} jc ifconfig -a".split()).output_from_json()
+    def get_interfaces_in_namespace(namespace_name: str) -> list:
+        """
+        Returns all interfaces that belong to a network namespace, a la ifconfig
+        """
+        return run_command(f"ip netns exec {namespace_name} jc ifconfig -a".split()).output_from_json()
 
     @staticmethod
     def destroy_namespace(namespace_name: str):
+        """
+        Destroys a network namespace, killing all processes inside and
+        moving interfaces back to the root namespace as necessary.
+        """
         if not NetworkNamespace.namespace_exists(namespace_name):
             raise NetworkNamespaceNotFoundError()
-        NetworkNamespace._logger.info(f"Asked to destroy namespace {namespace_name}")
-        NetworkNamespace._logger.info(f"Killing old processes in {namespace_name}")
+        NetworkNamespace._static_logger.info(f"Asked to destroy namespace {namespace_name}")
+        NetworkNamespace._static_logger.info(f"Killing old processes in {namespace_name}")
 
         # This particular subcommand doesn't support JSON mode.
-        pids = run_command(f"ip netns pids {namespace_name}".split()).output.split("\n")
-        pids = list(filter(None, pids))
+        pids = NetworkNamespace.processes_using_namespace(namespace_name)
 
         for pid in pids:
-            NetworkNamespace._logger.info(f"Killing process {pid}")
+            NetworkNamespace._static_logger.info(f"Killing process {pid}")
             res = run_command(f"ip netns exec {namespace_name} kill {pid}".split(), raise_on_fail=False)
             if not res.success:
                 raise NetworkNamespaceError(f"Failed to kill process {pid} while destroying namespace {namespace_name}")
 
-        NetworkNamespace._logger.info(f"Moving interfaces out of {namespace_name}")
+        NetworkNamespace._static_logger.info(f"Moving interfaces out of {namespace_name}")
 
         for interface in NetworkNamespace.get_interfaces_in_namespace(namespace_name):
             print(interface)
-            NetworkNamespace._logger.info(f"Moving interface {interface} out of {namespace_name}")
+            NetworkNamespace._static_logger.info(f"Moving interface {interface} out of {namespace_name}")
 
 
             if interface['name'].startswith('wlan'):
@@ -82,11 +128,11 @@ class NetworkNamespace():
                     raise NetworkNamespaceError(f"Failed to move wired interface {interface['name']} to default namespace: {res.error}")
 
             elif interface['name'].startswith('lo'):
-                NetworkNamespace._logger.info(f"Ignoring loopback interface {interface['name']}")
+                NetworkNamespace._static_logger.info(f"Ignoring loopback interface {interface['name']}")
             else:
                 raise NetworkNamespaceError(f"Don't know how to move {interface['name']} to default namespace.")
 
-        NetworkNamespace._logger.info(f"Deleting namespace {namespace_name}")
+        NetworkNamespace._static_logger.info(f"Deleting namespace {namespace_name}")
         res = run_command(f"ip netns del {namespace_name}".split(),
                           raise_on_fail=False)
         if not res.success:
@@ -94,7 +140,10 @@ class NetworkNamespace():
 
     @staticmethod
     def processes_using_namespace(namespace_name: str):
+        """
+        Lists all processes currently running in a namespace
+        """
         result = run_command(f"ip netns pids {namespace_name}".split(), raise_on_fail=False)
         if not result.success:
             raise NetworkNamespaceError(f"Error getting namespace processes: {result.error}")
-        return list(filter(None, result.output.split('\n') or []))
+        return [int(x) for x in filter(None, result.output.split('\n') or [])]
